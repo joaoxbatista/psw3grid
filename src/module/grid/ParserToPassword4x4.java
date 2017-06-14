7394d12a295ed395bd620443ada387940cdbf603
Nayane <nay.frez@gmail.com>
2017-06-14 13:15:11 -0300

    adicionei grid 4x4

---
+++ src/module/grid/ParserToPassword4x4.java
@@ -0,0 +1,61 @@
+package module.grid;
+
+public class ParserToPassword4x4 extends ParserToPassword {
+	
+	public static String parse(String commands_input, Grid grid) {
+		String[] commands = commands_input.split(";");
+		
+		String password = "";
+		for (int i = 0; i < commands.length; i++) {
+
+			switch (commands[i]) {
+			case "c1":
+
+				password += grid.getCollumn(1, 1);
+
+				break;
+
+			case "c2":
+				password += grid.getCollumn(2, 1);
+				break;
+
+			case "c3":
+				password += grid.getCollumn(3, 1);
+				break;
+			
+			case "c4":
+				password += grid.getCollumn(4, 1);
+				break;
+				
+				
+
+			case "r1":
+				password += grid.getLine(1, 1);
+				break;
+
+			case "r2":
+				password += grid.getLine(2, 1);
+				break;
+
+			case "r3":
+				password += grid.getLine(3, 1);
+				break;
+				
+			case "r4":
+				password += grid.getLine(4, 1);
+				break;
+
+			case "dp":
+				password += grid.getDiagonal(1, 1);
+				break;
+
+			case "ds":
+				password += grid.getDiagonal(2, 1);
+				break;
+
+			}
+		}
+		return password;
+	}
+
+}
